91bd9e8f08df06ba09eba8e1e47cdcae994024ac
Olivier Cartier <cestoliv@chevro.fr>
2020-10-11 18:00:40 +0200

    better interface :) I renamed the function show_warnings to show_infos. show_infos takes care of modifying the frame (text ...) and the main loop takes care of the display.

--- QR_Code_reader_05.py
+++ QR_Code_reader_05.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import pyzbar.pyzbar as pyzbar
 import datetime
 import time
@@ -7,7 +8,7 @@ from pydub.playback import play
 
 from db.db import DBConnection
 
-
+beep = AudioSegment.from_file("beep.wav")
 
 def purge_alreadyScanned(alreadyScanned):
     """
@@ -32,8 +33,7 @@ def capture_qrcode():
     """
     _, frame = cap.read() # frame capture
     flipped_frame=cv2.flip(frame, 1)
-    cv2.imshow("Frame", flipped_frame) # frame display
-    return pyzbar.decode(frame) # return qr codes
+    return flipped_frame, pyzbar.decode(flipped_frame) # return qr codes
 
 def add_to_db(id_e,timestamp):
     """
@@ -56,9 +56,87 @@ def add_to_db(id_e,timestamp):
 
     DB.dump()
 
-def show_warning(status):
-    pass
+def show_infos(qr_code_object, frame, scan_time): 
+    """
+        Takes the QrCode object from pyzbar, 
+        the frame and the date of the scan. 
+        Returns the (modified) frame.
+        If the scan date has passed for less than 3 seconds,
+        "Scanne!" is displayed in green. If it has been 
+        exceeded for more than 3 seconds, 
+        "Scanne il y a moins de 5 mn" is displayed in orange.
+        -----
+        Prend l'objet QrCode de pyzbar, 
+        la frame et la date du scan. 
+        Retourne la frame (modifiée).
+        Si la date du scan est dépassée depuis moins de 3 secondes, 
+        on affiche "Scanne !" en vert. Si elle est dépassée 
+        depuis plus de 3 secondes, on affiche 
+        "Scanne il y a moins de 5 mn" en orange.
+    """
 
+
+    if not isinstance(qr_code_object, pyzbar.Decoded):
+        return frame
+    if not isinstance(frame, np.ndarray):
+        return frame
+    if not isinstance(scan_time, datetime.datetime):
+        return frame
+
+    pts = np.array([
+        [
+            qr_code_object.polygon[0].x,
+            qr_code_object.polygon[0].y
+        ],
+        [
+            qr_code_object.polygon[1].x,
+            qr_code_object.polygon[1].y
+        ],
+        [
+            qr_code_object.polygon[2].x,
+            qr_code_object.polygon[2].y
+        ],
+        [
+            qr_code_object.polygon[3].x,
+            qr_code_object.polygon[3].y
+        ]
+    ], np.int32) # creates an array of polygon points from the QrCode
+    pts = pts.reshape((-1,1,2))
+
+    # default
+    color = (0, 165, 255) # orange
+    text = "Scanne il y a moins de 5 mn"
+    
+    # if the scan date has passed for less than 3 seconds
+    time_between_scan_and_now = (datetime.datetime.now() - scan_time).total_seconds()
+    if time_between_scan_and_now < 3:
+        color = (0, 255, 0) # green
+        text = "Scanne !"
+
+
+    frame = cv2.polylines(frame, [pts], True, color, thickness=5) # draw the outline of the qrcode
+
+    labelSize=cv2.getTextSize(text,cv2.FONT_HERSHEY_COMPLEX,0.5,2) # calculating the text size on the screen
+
+    labelX = int(qr_code_object.rect.left + qr_code_object.rect.width / 2 - labelSize[0][0]/2) # text X
+    labelY = int(qr_code_object.rect.top + qr_code_object.rect.height + 30) # text Y
+
+    padding = 8
+
+    frame = cv2.rectangle(frame,
+        (
+            labelX - padding,
+            labelY + padding
+        ),
+        (
+            labelX + int(labelSize[0][0]) + padding,
+            labelY - int(labelSize[0][1]) - padding
+        )
+        , color, cv2.FILLED) # draw a background for the text
+
+    frame = cv2.putText(frame, text, (labelX, labelY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA) # draw the text
+    
+    return frame
 
 
 # Connection to database
@@ -77,7 +155,7 @@ while True:
     alreadyScanned=purge_alreadyScanned(alreadyScanned)
 
     #Affichage de l'image et récupération des codes
-    decodedObjects=capture_qrcode()
+    frame, decodedObjects=capture_qrcode()
 
     #Récupération de l'heure du scan
     scan_time = datetime.datetime.now()
@@ -91,10 +169,12 @@ while True:
             
             #MAJ de la BDD
             add_to_db(id_eleve, scan_time)
+            play(beep)
 
-            show_warning('ok')
-        else :
-            show_warning('nok')
+        real_scan_time = alreadyScanned[id_eleve]-datetime.timedelta(minutes=5) # the time_scan without the added 5mins
+        frame = show_infos(obj, frame, real_scan_time) # update the frame with the text...
+
+    cv2.imshow("Frame", frame) # frame display
             
     #Capture d'un appui de touche sur le clavier
     key = cv2.waitKey(1)
@@ -104,9 +184,6 @@ while True:
         break
 
 
-
-# conda install opencv
-# pip install pyzbar
-# pip install pydub
+# pip install -r requirements.txt
 
 # https://www.sqlshack.com/python-scripts-to-format-data-in-microsoft-excel/
